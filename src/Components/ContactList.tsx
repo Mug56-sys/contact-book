1e4a83ece8a3f744b108b72fad0bd76e2ec0a606
Mug56-sys <kacper.bandula29@gmail.com>
2025-07-26 16:25:24 +0200

    Adding ContactList Component and grid

---
+++ src/Components/ContactList.tsx
@@ -0,0 +1,24 @@
+
+export default function ContactList() {
+  return (
+    <div className=" h-screen grid grid-cols-3 auto-rows-[200px] gap-3 mt-3 pb-5">
+      <div className="border rounded-xl bg-white shadow-lg">
+        <div className="p-3">
+      <h1 className="text-xl font-semibold">Name:</h1>
+      <p className="pt-1 text-sm">City:</p>
+        </div>
+        </div>
+      <div className="border">2</div>
+      <div className="border">3</div>
+      <div className="border">1</div>
+      <div className="border">2</div>
+      <div className="border">3</div>
+      <div className="border">1</div>
+      <div className="border">2</div>
+      <div className="border">3</div>
+      <div className="border">1</div>
+      <div className="border">2</div>
+      <div className="border">3</div>
+    </div>
+  )
+}
