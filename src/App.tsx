14c09d6930468f7e98c8068c4ea67b273ad60910
Mug56-sys <kacper.bandula29@gmail.com>
2025-07-27 21:38:09 +0200

    Adding add, delete functions, transfering ContactList and contact type to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,62 @@
 import "./index.css";
 import ContactList from "./Components/ContactList";
+import { useEffect, useState } from "react";
+
+export type Contact={
+ id:number;
+ name:string;
+ city:string;
+}
 
 function App() {
+  
+const devList:Contact[]=[
+    { id: 1, name: "Alice Johnson", city: "New York" },
+    { id: 2, name: "Bob Smith", city: "Los Angeles" },
+    { id: 3, name: "Charlie Brown", city: "Chicago" },
+    { id: 4, name: "David Williams", city: "Houston" },
+   
+]
+
+  const [name,SetName]=useState<string>('')
+  const [city,SetCity]=useState<string>('')
+  const [contactList,SetContactList]=useState<Contact[]>([])
+ 
+  const idLength=contactList.length;
+  console.log(idLength) 
+
+useEffect(()=>{
+  SetContactList(devList)
+  
+  },[])
+  
+  const handleSubmit=()=>{
+    let id=1;
+    while(contactList.some((contact:Contact)=>contact.id===id)){
+      id++
+    }
+    SetContactList([...contactList,{id,name,city}])
+    SetName('');
+    SetCity('');
+  }
+
   return (
     <div className="h-100% bg-gray-300 pt-5 px-6 ">
       <h1 className="text-3xl font-semibold">Contact Book</h1>
       <span className="text-sm ">Keep track of where your friends live in</span>
       <div className="bg-white shadow-md w-7/18 min-w-65 max-w-150 py-4 mt-3 rounded-lg  px-2 ">
       <div className="flex justify-evenly items-center py-1">
-        <input placeholder="Name" className="border p-1 pl-2 rounded-md text-sm w-1/3"/>
-        <input placeholder="City" className="border p-1 pl-2 rounded-md text-sm w-1/3"/>
-        <button className="bg-violet-600 text-nowrap text-white rounded-lg py-2 px-3 text-xs cursor-pointer ">Add Contact</button>
+        <form id="Add" onSubmit={(e)=>{
+          e.preventDefault()
+          handleSubmit();
+        }}>
+        <input placeholder="Name" className="border p-1 pl-2 rounded-md text-sm w-1/3" value={name} onChange={(e)=>SetName(e.target.value)} required/>
+        <input placeholder="City" className="border p-1 pl-2 rounded-md text-sm w-1/3" value={city} onChange={(e)=>SetCity(e.target.value)} required/>
+        <button id="Add" className="bg-violet-600 text-nowrap text-white rounded-lg py-2 px-3 text-xs cursor-pointer ">Add Contact</button>
+        </form>
         </div>
       </div>
-      <ContactList/>
+      <ContactList SetContactList={SetContactList} contactList={contactList} setName={SetName} setCity={SetCity}/>
     </div>
   );
 }
--- src/Components/ContactList.tsx
+++ src/Components/ContactList.tsx
@@ -1,43 +1,65 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { type Contact } from "../App";
 
-export default function ContactList() {
-  const [editing,SetEditing]=useState(false)
 
-  const handleClick=()=>{
+
+export default function ContactList({SetContactList,contactList,setName,setCity}:{SetContactList:React.Dispatch<React.SetStateAction<Contact[]>>;contactList:Contact[];setName:React.Dispatch<React.SetStateAction<string>>;setCity:React.Dispatch<React.SetStateAction<string>>;}) {
+ 
+ 
+  const [editing,SetEditing]=useState<number | null>(null)
+  const [editObj,setEditObj]=useState()
+  
+  const handleClick=(id:number)=>{
+    SetEditing(id)
       console.log(`click`)
   }
-  const handleDelete=()=>{
+  const handleDelete=(id:number)=>{
+    
+    SetEditing(null)
+    const UpdatedList:Contact[]=contactList.filter(contact=>contact.id!==(id))
+    SetContactList(UpdatedList)
+   
     console.log(`deleting`)
   } 
   const handleSave=()=>{
+    //SetContactList({id,})
+    SetEditing(null)
     console.log(`Saving`)
   }
   const handleCancel=()=>{
+    SetEditing(null)
      console.log(`canceling`)
   }
   return (
-    <div className=" h-screen grid grid-cols-3 auto-rows-[200px] gap-3 mt-3 pb-5">
-      {!editing ?<div className=" rounded-xl bg-white shadow-lg">
+    <div className=" h-screen grid grid-cols-3 auto-rows-[200px] gap-3 mt-3 pb-5 ">
+      {contactList ? contactList.map((contact:Contact)=>(
+        <div className=" rounded-xl bg-white shadow-lg " key={contact.id}>
+        {!(editing===contact.id) ?
         <div className="p-3 relative  size-full  ">
-          <h1 className="text-xl font-semibold">Name:</h1>
-          <p className="pt-1 text-sm">City:</p>
-          <button onClick={handleClick} className=" px-5 py-1 rounded-lg bg-gray-300 cursor-pointer absolute right-5 bottom-5">
+          <h1 className="text-xl font-semibold">Name:{contact.name}</h1>
+          <p className="pt-1 text-sm">City:{contact.city}</p>
+          <button onClick={()=>handleClick(contact.id)} className=" px-5 py-1 rounded-lg bg-gray-300 cursor-pointer absolute right-5 bottom-5">
             Edit
           </button>
         </div>
-      </div>:<div className=" rounded-xl bg-white shadow-lg ">
+      :
+      
         <div className="pt-5 px-4  size-full relative  size-full flex-row">
           <input
             placeholder="Name"
             name="Full-name"
             className="border p-1 pl-2 rounded-md text-sm w-full"
+            value={contact.name}
+            onChange={(e)=>{}}
           />
           <input
             placeholder="City"
             name="City"
             className="border p-1 pl-2 rounded-md text-sm w-full  mt-2"
+            value={contact.city}
+            onChange={(e)=>{}}
           />
-          <button onClick={handleDelete} className=" px-2 py-1 rounded-lg bg-red-400 text-white cursor-pointer absolute left-4 bottom-5">
+          <button onClick={()=>handleDelete(contact.id)} className=" px-2 py-1 rounded-lg bg-red-400 text-white cursor-pointer absolute left-4 bottom-5">
             Delete
           </button>
           <button onClick={handleSave} className=" px-2 py-1 rounded-sm bg-violet-600 text-white cursor-pointer absolute right-5 bottom-5">
@@ -47,71 +69,11 @@ export default function ContactList() {
             Cancel
           </button>
         </div>
-      </div>}
+     }
+        </div>
+      )):null}
+      
 
-      {!editing ?<div className=" rounded-xl bg-white shadow-lg">
-        <div className="p-3 relative  size-full  ">
-          <h1 className="text-xl font-semibold">Name:</h1>
-          <p className="pt-1 text-sm">City:</p>
-          <button onClick={handleClick} className=" px-5 py-1 rounded-lg bg-gray-300 cursor-pointer absolute right-5 bottom-5">
-            Edit
-          </button>
-        </div>
-      </div>:<div className=" rounded-xl bg-white shadow-lg ">
-        <div className="pt-5 px-4  size-full relative  size-full flex-row">
-          <input
-            placeholder="Name"
-            name="Full-name"
-            className="border p-1 pl-2 rounded-md text-sm w-full"
-          />
-          <input
-            placeholder="City"
-            name="City"
-            className="border p-1 pl-2 rounded-md text-sm w-full  mt-2"
-          />
-          <button onClick={handleDelete} className=" px-2 py-1 rounded-lg bg-red-400 text-white cursor-pointer absolute left-4 bottom-5">
-            Delete
-          </button>
-          <button onClick={handleSave} className=" px-2 py-1 rounded-sm bg-violet-600 text-white cursor-pointer absolute right-5 bottom-5">
-            Save
-          </button>
-          <button onClick={handleCancel} className=" px-2 py-1 rounded-sm bg-gray-200 text-black cursor-pointer absolute right-18 bottom-5">
-            Cancel
-          </button>
-        </div>
-      </div>}
-
-      {!editing ?<div className=" rounded-xl bg-white shadow-lg">
-        <div className="p-3 relative  size-full  ">
-          <h1 className="text-xl font-semibold">Name:</h1>
-          <p className="pt-1 text-sm">City:</p>
-          <button onClick={handleClick} className=" px-5 py-1 rounded-lg bg-gray-300 cursor-pointer absolute right-5 bottom-5">
-            Edit
-          </button>
-        </div>
-      </div>:<div className=" rounded-xl bg-white shadow-lg ">
-        <div className="pt-5 px-4  size-full relative  size-full flex-row">
-          <input
-            placeholder="Name"
-            name="Full-name"
-            className="border p-1 pl-2 rounded-md text-sm w-full"
-          />
-          <input
-            placeholder="City"
-            name="City"
-            className="border p-1 pl-2 rounded-md text-sm w-full  mt-2"
-          />
-          <button onClick={handleDelete} className=" px-2 py-1 rounded-lg bg-red-400 text-white cursor-pointer absolute left-4 bottom-5">
-            Delete
-          </button>
-          <button onClick={handleSave} className=" px-2 py-1 rounded-sm bg-violet-600 text-white cursor-pointer absolute right-5 bottom-5">
-            Save
-          </button>
-          <button onClick={handleCancel} className=" px-2 py-1 rounded-sm bg-gray-200 text-black cursor-pointer absolute right-18 bottom-5">
-            Cancel
-          </button>
-        </div>
-      </div>}
       
     </div>
   );
